d8d977bb003b2d9d095b0ec4f9f1e483e16a73ad
Iwo Stanisławski <iwo.stanislawski@icloud.com>
2025-05-09 04:20:03 +0200

    Init commit

---
+++ zadanko/src/main/java/org/example/zadanko/ZadankoApplication.java
@@ -0,0 +1,13 @@
+package org.example.zadanko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadankoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadankoApplication.class, args);
+    }
+
+}
---
+++ zadanko/src/test/java/org/example/zadanko/ZadankoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.zadanko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadankoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
