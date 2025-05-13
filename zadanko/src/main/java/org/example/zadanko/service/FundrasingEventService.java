6094a11e00e33c46a5d32b9b145dd9a901f8ae1f
Iwo Stanisławski <iwo.stanislawski@icloud.com>
2025-05-13 23:18:30 +0200

    Clearing

--- zadanko/src/main/java/org/example/zadanko/service/FundrasingEventService.java
+++ zadanko/src/main/java/org/example/zadanko/service/FundrasingEventService.java
@@ -14,7 +14,6 @@ import org.example.zadanko.repository.FundraisingEventRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
