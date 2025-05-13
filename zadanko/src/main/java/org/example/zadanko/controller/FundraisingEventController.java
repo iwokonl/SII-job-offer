5888f24dfb3d8d0c9e5a6bf77c5faf4493c5e443
Iwo Stanisławski <iwo.stanislawski@icloud.com>
2025-05-13 23:10:03 +0200

    Update FundraisingEventController.java

--- zadanko/src/main/java/org/example/zadanko/controller/FundraisingEventController.java
+++ zadanko/src/main/java/org/example/zadanko/controller/FundraisingEventController.java
@@ -1,25 +1,45 @@
 package org.example.zadanko.controller;
 
 
-import org.example.zadanko.dto.CreatedFundrasingEventDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.zadanko.dto.CreatedFundrasingEvent.CreatedFundrasingEventRequestDto;
+import org.example.zadanko.dto.CreatedFundrasingEvent.CreatedFundrasingEventResponseDto;
+import org.example.zadanko.dto.FinancialReport.FinacialReportResponseDto;
+import org.example.zadanko.service.FundrasingEventService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
-@RequestMapping("/api/fundraising-event")
+@RequestMapping("/api/fundraisingEvent")
+@RequiredArgsConstructor
 public class FundraisingEventController {
 
-
+    private final FundrasingEventService fundraisingEventService;
 
     @PostMapping("/create")
-    public ResponseEntity<CreatedFundrasingEventDto> crateFundraisingEvent(){
-        return ResponseEntity.ok(new CreatedFundrasingEventDto());
+    public ResponseEntity<CreatedFundrasingEventResponseDto> crateFundraisingEvent(@RequestBody @Valid CreatedFundrasingEventRequestDto createdFundrasingEventRequestDto) {
+        return ResponseEntity.ok(fundraisingEventService.createFundraisingEvent(createdFundrasingEventRequestDto));
     }
 
-    @PutMapping("/assign-box")
+    @PutMapping("/removeBox")
     @ResponseStatus(HttpStatus.OK)
-    public void assignBoxToFundraisingEvent(){
+    public void removeBox(@RequestParam("boxId") UUID boxId) {
+        fundraisingEventService.removeBox(boxId);
+    }
 
+    @PutMapping("/assignBox")
+    @ResponseStatus(HttpStatus.OK)
+    public void assignBoxToFundraisingEvent(@RequestParam("boxId") UUID boxId, @RequestParam("fundraisingEventId") UUID fundraisingEventId) {
+        fundraisingEventService.assignBox(boxId, fundraisingEventId);
+    }
+
+    @GetMapping("/financialReport")
+    public ResponseEntity<List<FinacialReportResponseDto>> createFinancialReport() {
+        return ResponseEntity.ok(fundraisingEventService.createFinancialReport());
     }
 }
