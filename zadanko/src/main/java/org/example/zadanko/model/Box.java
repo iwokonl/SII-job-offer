f910e994c8eadf987f45b96fba29559bb8e06fb0
Iwo Stanisławski <iwo.stanislawski@icloud.com>
2025-05-13 23:12:22 +0200

    Update Box.java

--- zadanko/src/main/java/org/example/zadanko/model/Box.java
+++ zadanko/src/main/java/org/example/zadanko/model/Box.java
@@ -1,23 +1,35 @@
 package org.example.zadanko.model;
 
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Box {
 
     @Id
     @GeneratedValue
     private UUID id;
     @ElementCollection
-    private Map<Currency, BigDecimal> currencyMap;
+    private Map<Currency, BigDecimal> ballanceMap;
 
+    @OneToOne(mappedBy = "box", cascade = CascadeType.ALL)
+    private FundraisingEvent fundraisingEvent;
 
+    @PreUpdate
+    private void checkAssignmentBeforeUpdate() {
+        if (fundraisingEvent == null) {
+            ballanceMap.clear();
+        }
+    }
 }
